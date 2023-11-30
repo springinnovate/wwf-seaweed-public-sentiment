0645d38af38ce7416f26fb88833f5d5374b4777f
Richard Sharp <richpsharp@gmail.com>
2023-11-29 18:51:11 -0800

    parametrizing so i can pass search files

--- reddit_api_tracer.py
+++ reddit_api_tracer.py
@@ -1,12 +1,9 @@
 """Tracer code to search a subreddit."""
+import argparse
 import logging
 import sys
 
 import praw
-
-
-USER_AGENT = 'python:wwf-seaweed-sentiment-bot:0.0 (by /u/wwf-seaweed-sentiment-bot)'
-
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -18,28 +15,36 @@ LOGGER = logging.getLogger(__name__)
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Search Reddit')
+    parser.add_argument('search_file')
+    args = parser.parse_args()
+
+    with open(args.search_file, 'r') as search_file:
+        subreddit_list, search_query = search_file.read().rstrip().split('\n')
+        subreddit_list = subreddit_list.split(' ')
+
     with open('secrets.txt', 'r') as secret_file:
-        client_id, client_secret = secret_file.read().split('\n')
+        client_id, client_secret, username, password = \
+            secret_file.read().rstrip().split('\n')
 
     LOGGER.debug(f'client: {client_id}, secret: {client_secret}')
 
     reddit = praw.Reddit(
         client_id=client_id,
         client_secret=client_secret,
-        user_agent=f'python:{client_id}:0.0 (by /u/{client_id})'
+        password=password,
+        username=username,
+        user_agent=f'python:wwf-seaweed-sentiment-bot:0.0 (by /u/{username})'
     )
 
     # Define the subreddit and the search query
-    subreddit_name = 'Maine'
-    search_query = 'seaweed'
+    for subreddit_name in subreddit_list:
+        subreddit = reddit.subreddit(subreddit_name)
+        for index, submission in enumerate(subreddit.search(
+                search_query, sort='relevance', limit=100)):
+            LOGGER.info(
+                f'{index} -- TITLE: {submission.title}, DATETIME: {submission.created_utc}')
 
-    # Access the subreddit
-    subreddit = reddit.subreddit(subreddit_name)
-
-    # Search the subreddit for the query and fetch titles and dates
-    for submission in subreddit.search(search_query, limit=10):
-        LOGGER.info(
-            f'TITLE: {submission.title}, DATETIME: {submission.created_utc}')
 
 if __name__ == '__main__':
     main()
