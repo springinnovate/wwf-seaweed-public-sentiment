5fc0d88d1357098d2414017b2b2a0a08978dc1cf
Richard Sharp <richpsharp@gmail.com>
2024-01-10 17:23:40 -0800

    do albert

--- headline_sentiment_analysis_trainer.py
+++ headline_sentiment_analysis_trainer.py
@@ -22,9 +22,9 @@ with open('huggingface_tokens.txt', 'r', encoding='utf-8') as file:
 #MODEL_ID = 'bert-base-uncased'  # 0.81 accuracy in 2 epochs
 #MODEL_ID = 'bert-base-cased'  # 0.7705263157894737 accuracy in 2 epochs
 #MODEL_ID = 'roberta-base'  # 0.7789473684210526 accuracy in 2 epochs
-#MODEL_ID = 'albert-base-v2'
-#MODEL_ID = 'google/electra-base-generator' working well got up to 0.84 after 20 or so
-MODEL_ID = 'microsoft/deberta-v3-base'
+#MODEL_ID = 'google/electra-base-generator' working well got up to 0.84 after 5 or so
+#MODEL_ID = 'microsoft/deberta-v3-base'  # .865 after 6 epoch
+MODEL_ID = 'albert-base-v2'
 TOKENIZER = AutoTokenizer.from_pretrained(
     MODEL_ID, token=access_token_write)
 DATA_COLLATOR = DataCollatorWithPadding(tokenizer=TOKENIZER)
