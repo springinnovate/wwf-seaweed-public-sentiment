046ba958aa692a7d327bada3375a00137dc1d218
Rich Sharp <richpsharp@gmail.com>
2024-01-29 09:04:21 -0800

    doing headline sentiment right in the model

--- parse_docx_articles.py
+++ parse_docx_articles.py
@@ -1,12 +1,25 @@
 """Tracer code to figure out how to parse out DocX files."""
 import argparse
 import glob
+import re
 import time
 
+import pandas
 from docx import Document
+from transformers import pipeline
+from concurrent.futures import ThreadPoolExecutor
+
+MODEL_PATH = 'wwf-seaweed-headline-sentiment/microsoft-deberta-v3-base_6'
+HEADLINE_LABEL_TO_SENTIMENT = {
+    'LABEL_0': 'bad',
+    'LABEL_1': 'neutral',
+    'LABEL_2': 'good',
+}
 
 
-def read_docx(file_path):
+def parse_docx(file_path, headline_sentiment_model):
+    start_time = time.time()
+    print(f'parsing {file_path}')
     class Parser:
         def __init__(self):
             doc = Document(file_path)
@@ -22,10 +35,13 @@ def read_docx(file_path):
                     return val
 
     paragaph_iter = Parser()
-    for text in paragaph_iter:
+    while True:
+        text = next(paragaph_iter)
         if text != '':
-            headline = text
             break
+    headline_text = text
+    publication_text = next(paragaph_iter)
+    date_text = next(paragaph_iter)
     for text in paragaph_iter:
         if text == 'Body':
             break
@@ -36,20 +52,34 @@ def read_docx(file_path):
             break
         body_text += text
 
-    custom_tags = {
-        'Subject': '',
-        'Industry': '',
-        'Geographic': '',
-        'Load-Date': '',
+    result = {
+        'subject': '',
+        'industry': '',
+        'geographic': '',
+        'load-date': '',
+        'headline': headline_text,
+        'body': body_text,
+        'publication': publication_text,
+        'date': date_text,
     }
+
+    try:
+        result['year'] = re.search(r'\d{4}', date_text).group()
+    except AttributeError:
+        result['year'] = 'unknown'
 
     for text in paragaph_iter:
         tag = text.split(':')[0]
-        if tag in custom_tags:
-            custom_tags[tag] = text
-    return {
-        'Body': body_text
-    }.update(custom_tags)
+        if tag in result:
+            result[tag] = text
+
+    headline_sentiment = headline_sentiment_model([headline_text])[0]
+    result['headline-sentiment'] = HEADLINE_LABEL_TO_SENTIMENT[headline_sentiment['label']]
+    result['headline-sentiment-score'] = headline_sentiment['score']
+
+    print(f'time to parse = {time.time()-start_time}s')
+
+    return result
 
 
 def main():
@@ -57,11 +87,20 @@ def main():
     parser.add_argument('path_to_files', help='Path/wildcard to docx files')
     args = parser.parse_args()
 
-    start_time = time.time()
-    for index, file_path in enumerate(glob.glob(args.path_to_files)):
-        print(f'parsing {index} {file_path}')
-        print(f'avg time = {(time.time()-start_time)/(index+1)}')
-        read_docx(file_path)
+    print(f'load {MODEL_PATH}')
+    model = pipeline(
+        'sentiment-analysis', model=MODEL_PATH, device='cuda')
+    print('loaded...')
+
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        future_list = []
+        for index, file_path in enumerate(glob.glob(args.path_to_files)):
+            future = executor.submit(parse_docx, file_path, model)
+            future_list.append(future)
+            if index > 4:
+                break
+        df = pandas.DataFrame.from_records([future.result() for future in future_list])
+        df.to_csv('_parse_doc.csv')
 
 
 if __name__ == '__main__':
