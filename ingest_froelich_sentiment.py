0f07bba59777774ceda5d13cea7393a0c2dd300d
Rich Sharp <richpsharp@gmail.com>
2024-02-06 13:51:13 -0800

    track script to add pre-known sentiment

---
+++ ingest_froelich_sentiment.py
@@ -0,0 +1,59 @@
+"""Inject Froelich summaries"""
+import pandas
+
+from database_model_definitions import Article
+from database_operations import upsert_articles
+from database import SessionLocal, init_db
+from sqlalchemy import func
+
+MODEL_PATH = 'wwf-seaweed-headline-sentiment/microsoft-deberta-v3-base_6'
+HEADLINE_LABEL_TO_SENTIMENT = {
+    -1: 'bad',
+    0: 'neutral',
+    1: 'good',
+}
+
+
+def main():
+    init_db()
+    session = SessionLocal()
+    print('updating headline sentiment in database')
+
+    punctuations = ['.', ',', '!', '?', ';', ':', '-', '_']
+
+    # Starting point for our filter
+    filter_expression = func.lower(Article.headline)
+
+    # Dynamically build the filter expression to remove punctuation
+    for p in punctuations:
+        filter_expression = func.replace(filter_expression, p, '')
+
+    froelich_table = pandas.read_csv("data/papers/froelich_headlines.csv")
+    new_article_list = []
+    for index, row in froelich_table.iterrows():
+        # Do the same for the search string
+        search_string = row['headline'].lower()
+        froelich_sentiment = HEADLINE_LABEL_TO_SENTIMENT[row['sentiment']]
+        for p in punctuations:
+            search_string = search_string.replace(p, '')
+
+        matching_articles = session.query(Article).filter(
+            func.lower(Article.headline) == func.lower(row['headline'])).all()
+        for article in matching_articles:
+            article.ground_truth_headline_sentiment = froelich_sentiment
+        if matching_articles == []:
+            # insert a new article
+            new_article = Article(
+                headline=row['headline'],
+                ground_truth_headline_sentiment=froelich_sentiment,
+                source_file='data/papers/froelich_headlines.csv',
+                )
+            new_article_list.append(new_article)
+
+    upsert_articles(session, new_article_list)
+    session.commit()
+    session.close()
+
+
+if __name__ == '__main__':
+    main()
