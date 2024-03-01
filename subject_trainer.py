80c67b8ef96ae8e27a407cda910c275d9f181d6b
Richard Sharp <richpsharp@gmail.com>
2024-02-29 16:39:06 -0800

    break it down to 1 item

--- subject_trainer.py
+++ subject_trainer.py
@@ -159,8 +159,8 @@ def main():
     repo_name = "wwf-seaweed-body-subject"
     training_args = TrainingArguments(
        output_dir=repo_name,
-       per_device_train_batch_size=16,
-       per_device_eval_batch_size=16,
+       per_device_train_batch_size=1,
+       per_device_eval_batch_size=1,
        num_train_epochs=1,
        weight_decay=0.01,
        save_strategy="epoch",
