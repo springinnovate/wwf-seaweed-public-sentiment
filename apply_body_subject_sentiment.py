159eeb69e398a74843ffe071496f47066e4071ab
Rich Sharp <richpsharp@gmail.com>
2024-04-06 13:15:35 -0700

    memoized body transform

--- apply_body_subject_sentiment.py
+++ apply_body_subject_sentiment.py
@@ -1,5 +1,6 @@
 """Tracer code to figure out how to parse out DocX files."""
 from transformers import pipeline
+from functools import wraps
 
 from database_model_definitions import Article, AIResultBody, USER_CLASSIFIED_BODY_OPTIONS, RELEVANT_SUBJECT_TO_LABEL, AQUACULTURE_SUBJECT_TO_LABEL, SEAWEED_LABEL, OTHER_AQUACULTURE_LABEL
 from database_model_definitions import RELEVANT_LABEL, IRRELEVANT_LABEL
@@ -21,17 +22,37 @@ RELEVANT_SUBJECT_MODEL_PATH = "wwf-seaweed-body-subject-relevant-irrelevant/alle
 AQUACULTURE_SUBJECT_MODEL_PATH = "wwf-seaweed-body-subject-aquaculture-type/allenai-longformer-base-4096_36"
 
 
+def memoize(func):
+    cache = {}
+    @wraps(func)
+    def memoized_func(*args):
+        if args in cache:
+            return cache[args]
+        result = func(*args)
+        cache[args] = result
+        return result
+    return memoized_func
+
+
+def make_pipeline(model_type, model_path):
+    _pipeline = pipeline(
+        model_type, model=model_path,
+        device='cuda', truncation=True)
+    @memoize
+    def _sentiment_op(text_val):
+        return _pipeline(text_val)
+    return _sentiment_op
+
+
 def main():
-    relevant_subject_model = pipeline(
-        'text-classification', model=RELEVANT_SUBJECT_MODEL_PATH,
-        device='cuda', truncation=True)
-    aquaculture_subject_model = pipeline(
-        'text-classification', model=AQUACULTURE_SUBJECT_MODEL_PATH,
-        device='cuda', truncation=True)
+    relevant_subject_model = make_pipeline(
+        'text-classification', RELEVANT_SUBJECT_MODEL_PATH)
+    aquaculture_subject_model = make_pipeline(
+        'text-classification', AQUACULTURE_SUBJECT_MODEL_PATH)
     print('loaded models...')
     init_db()
     session = SessionLocal()
-    articles_without_ai = [
+    articles_without_ai = (
         session.query(Article).outerjoin(
             AIResultBody, Article.id_key == AIResultBody.article_id)
         .filter(
@@ -39,7 +60,7 @@ def main():
             Article.body != None,
             Article.body != '')
         .limit(5)
-        .all()]
+        .all())
     bodies_without_ai = [article.body for article in articles_without_ai]
     print(f'doing sentiment-analysis on {len(bodies_without_ai)} headlines')
     relevant_subject_result_list = [
@@ -64,9 +85,6 @@ def main():
             articles_without_ai, relevant_subject_result_list):
         if relevant_subject_result['label'] == RELEVANT_TAG:
             continue
-        #print(body)
-        # same_body_articles = session.query(Article).filter(
-        #     Article.body == body).all()
         article.body_subject_ai = [
             AIResultBody(
                 value=relevant_subject_result['label'],
