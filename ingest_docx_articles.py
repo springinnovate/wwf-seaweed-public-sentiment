e1a7a35fda077b9eee55899e26df3c56356013c6
Richard P Sharp Jr <richpsharp@gmail.com>
2024-09-14 13:52:57 -0700

    got the whole batch parsing

--- ingest_docx_articles.py
+++ ingest_docx_articles.py
@@ -7,6 +7,7 @@ import time
 import re
 
 import pandas
+from striprtf.striprtf import rtf_to_text
 from concurrent.futures import as_completed
 from docx import Document
 from concurrent.futures import ProcessPoolExecutor
@@ -24,7 +25,6 @@ def parse_as_bibstyle(orignal_file_path, paragraph_iter):
         if text == 'End of Document':
             end_of_document = True
         elif end_of_document and text != 'Bibliography':
-            print(f'NEW ARTICLE ON THIS TEXT: {text}')
             headline_text = text
             break
         else:
@@ -34,7 +34,6 @@ def parse_as_bibstyle(orignal_file_path, paragraph_iter):
     article_list = []
     while True:  # parse all the articles in the docx
         try:
-            print(f'headline text: {headline_text}')
             publication_text = next(paragraph_iter)
             date_text = next(paragraph_iter)
             for text in paragraph_iter:
@@ -46,7 +45,6 @@ def parse_as_bibstyle(orignal_file_path, paragraph_iter):
                 if text == 'End of Document':
                     break
                 body_text += text
-            print(f'BODY: {body_text[:1000]}')
 
             new_article = Article(
                 headline=headline_text,
@@ -69,7 +67,6 @@ def parse_as_user_name(original_file_path, paragraph_iter):
     seen_number = False
     while True:
         text = next(paragraph_iter)
-        print(text)
         isdigit = text[0].isdigit()
         if seen_number and not isdigit:
             headline_text = text
@@ -77,7 +74,6 @@ def parse_as_user_name(original_file_path, paragraph_iter):
         if not seen_number and isdigit:
             seen_number = True
     headline_text = text
-    print(f'headline text: {headline_text}')
     article_list = []
     while True:
         try:
@@ -98,6 +94,7 @@ def parse_as_user_name(original_file_path, paragraph_iter):
                 body=body_text,
                 date=date_text,
                 publication=publication_text,
+                source_file=str(original_file_path)
             )
             article_list.append(new_article)
             headline_text = next(paragraph_iter)
@@ -107,8 +104,44 @@ def parse_as_user_name(original_file_path, paragraph_iter):
     return article_list
 
 
-def parse_docx(file_path):
+def parse_as_old_rtf(file_path, paragraph_iter):
     start_time = time.time()
+    seen_body = False
+    headline = ''
+    publication = ''
+    date = ''
+    body_text = ''
+    article_list = []
+    while True:
+        try:
+            line = next(paragraph_iter)
+            if line == 'End of Document':
+                if seen_body:
+                    seen_body = False
+                    new_article = Article(
+                        headline=headline,
+                        body=body_text,
+                        date=date,
+                        publication=publication,
+                        source_file=str(file_path),
+                    )
+                    article_list.append(new_article)
+                else:
+                    headline = next(paragraph_iter)
+                    publication = next(paragraph_iter)
+                    date = next(paragraph_iter)
+            else:
+                body_text += line
+            if line == 'Body':
+                seen_body = True
+                body_text = ''
+        except StopIteration:
+            break
+    print(f'time to parse = {time.time()-start_time}s')
+    return article_list
+
+
+def parse_docx(file_path):
     print(f'parsing {file_path}')
 
     class Parser:
@@ -132,12 +165,7 @@ def parse_docx(file_path):
     elif first_line.startswith('User Name: ='):
         return parse_as_user_name(file_path, paragraph_iter)
     else:
-        print(f'"{file_path}" has unknown first line: {first_line}')
-        return
-
-
-def parse_rtf(doc_path):
-    pass
+        return parse_as_old_rtf(file_path, paragraph_iter)
 
 
 def parse_csv(csv_path):
@@ -147,52 +175,67 @@ def parse_csv(csv_path):
     for item_title in possible_item_titles:
         if item_title in df.columns:
             break
-    print(df)
     grouped = df.groupby([item_title, 'item_pub_date', 'item_url'])
-    print(grouped)
 
     # Concatenate the 'item_text's into 'body', ignoring NaNs and empty strings
     df_combined = grouped['item_text'].apply(
         lambda x: ' '.join(x.dropna().astype(str).str.strip())
     ).reset_index(name='body')
 
-    # Display the combined DataFrame
-    print("\nCombined DataFrame:")
-    print(df_combined)
-    return
+    article_list = []
+    for index, row in df_combined.iterrows():
+        new_article = Article(
+            headline=row[item_title],
+            body=row['item_text'],
+            date=row['item_pub_date'],
+            source_file=str(csv_path),
+        )
+        article_list.append(new_article)
+    return article_list
 
 
 def main():
     parser = argparse.ArgumentParser(description='parse docx')
-    parser.add_argument('path_to_files', nargs='+', help='Path/wildcard to docx files')
+    #parser.add_argument('path_to_files', nargs='+', help='Path/wildcard to docx files')
     args = parser.parse_args()
 
-    parse_csv('./data/papers/2024-Seaweed-Regional/seaweedRegionalSearch_Manual_ScrapedData2024.csv')
-    #parse_csv('./data/papers/2024-Seaweed-Regional/Results-SeaweedRegionalSearch2024.csv')
-    return
-    #parse_docx('data/papers/2024-Seaweed-NexisUni/NexisUni_Seaweed_2024.DOCX')
-    #parse_docx('data/papers/Aquaculture-NexisUni-2024/NexisUni_Aquaculture_2024_1-100.DOCX')
-    #parse_docx('data/papers/Aquaculture-NexisUni-2024/NexisUni_Aquaculture_2024_1-100.DOCX')
-    #parse_docx('data/papers/2024-Seaweed-NexisUni/NexisUni_Seaweed_2024.DOCX')
-    #return
-
-    doc_path_list = [
-        pdf_file_path
-        for pdf_file_glob in args.path_to_files
-        for pdf_file_path in Path().rglob(pdf_file_glob)]
-
-    for doc_path in doc_path_list:
-        if doc_path.lower().endswith('.docx'):
-            parse_docx(doc_path)
-        elif doc_path.lower().endswith('.rtf'):
-            parse_rtf(doc_path)
-        elif doc_path.lower().endswith('.csv'):
-            parse_csv(doc_path)
-
-    return
+    path_to_files = [
+        "data/papers/Aquaculture-Regional-2024/*.docx",
+        "data/papers/2024-Seaweed-AccessWorldNews/*.docx",
+        "data/papers/2024-Seaweed-NexisUni/*.docx",
+        "data/papers/2024-Seaweed-Regional/*.docx",
+        "data/papers/Aquaculture-AccessWorldNews-2024/*.docx",
+        "data/papers/Aquaculture-NexisUni-2024/*.docx",
+        "data/papers/Aquaculture-Regional-2024/*.docx",
+        "data/papers/2024-Seaweed-AccessWorldNews/*.csv",
+        "data/papers/2024-Seaweed-NexisUni/*.csv",
+        "data/papers/2024-Seaweed-Regional/*.csv",
+        "data/papers/Aquaculture-AccessWorldNews-2024/*.csv",
+        "data/papers/Aquaculture-NexisUni-2024/*.csv",
+    ]
 
     init_db()
     db = SessionLocal()
+    doc_path_list = [
+        pdf_file_path
+        for pdf_file_glob in path_to_files  # args.path_to_files
+        for pdf_file_path in Path().rglob(pdf_file_glob)]
+
+    article_list = []
+    for doc_path in doc_path_list:
+        if str(doc_path).lower().endswith('.docx'):
+            article_list.extend(parse_docx(doc_path))
+        elif str(doc_path).lower().endswith('.csv'):
+            article_list.extend(parse_csv(doc_path))
+        else:
+            raise RuntimeError(f'unknown file {doc_path}')
+    print(f'upserting {len(article_list)} articles')
+    upsert_articles(db, article_list)
+    db.commit()
+    db.close()
+
+    return
+
 
     with ProcessPoolExecutor() as executor:
         future_list = []
